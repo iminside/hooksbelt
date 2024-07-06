b5d638886438e365987a3c01d50b2703ef5384cc
Dan Chu <4urbanoff@gmail.com>
2024-07-06 11:01:08 +0300

    update useWeekdays

--- packages/use_weekdays/src/use_weekdays.ts
+++ packages/use_weekdays/src/use_weekdays.ts
@@ -3,22 +3,25 @@ import { useIntl } from '@hooksbelt/use-intl'
 
 export interface UseWeekdaysOptions {
 	startFrom?: number
+	startKey?: number
+	rotate?: number
 	type?: 'long' | 'short' | 'narrow'
 }
 
 export const useWeekdays = (locale: string, options = {} as UseWeekdaysOptions) => {
-	const { startFrom = 0, type = 'long' } = options
+	const { startFrom = 0, startKey = 0, rotate = 0, type = 'long' } = options
 	const intl = useIntl(locale, {
 		weekday: { type: 'DateTime', weekday: type },
 	})
 
 	return useMemo(() => {
-		const acc = Array.from({ length: 7 }, (_, key) => {
-			const name = intl.weekday(Date.UTC(2001, 0, key, 12))
+		const acc = Array.from({ length: 7 }, (_, i) => {
+			const key = startKey + i
+			const name = intl.weekday(Date.UTC(2001, 0, startFrom + i, 12))
 
 			return { key, name }
 		})
 
-		return [...acc.slice(startFrom), ...acc.slice(0, startFrom)]
-	}, [intl, startFrom])
+		return [...acc.slice(rotate), ...acc.slice(0, rotate)]
+	}, [intl, startFrom, startKey, rotate])
 }
